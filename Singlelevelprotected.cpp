5408962ca21ad3d1fea2d214fe6940b13b6f286f
Yash Thakur <yashthakur200529@gmail.com>
2026-05-02 22:23:06 +0530

    Add files via upload

---
+++ Singlelevelprotected.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+    protected:
+    int i, j;
+
+    public:
+    Base()
+    {
+        cout<<"Inside base constructor\n";
+        i = 0;
+        j = 0;
+        
+    }
+
+    ~Base()
+    {
+        cout<<"Inside Base Destructor\n";
+    }
+
+    void fun()
+    {
+        cout<<"Inside base fun\n";
+    }
+
+};
+
+class Derived : public Base
+{
+    public:
+    int x;
+
+    Derived()
+    {
+        cout<<"Inside Derived constructor\n";
+        x = 0;
+    }
+
+    ~Derived()
+    {
+        cout<<"Inside derived destructor\n";
+    }
+
+    void gun()
+    {
+        cout<<"Inside gun of derived\n";
+    }
+};
+int main()
+{
+    cout<<"Inside Main\n";
+
+    Derived dobj;
+    
+    cout<<"Size of Base class object is : "<<sizeof(Base)<<"\n";        //8 bytes
+    cout<<"Size of derived class object is : "<<sizeof(Derived)<<"\n";  //12 bytes
+
+    //cout<<dobj.i<<"\n";   // error
+    //cout<<dobj.j<<"\n";   // error
+    cout<<dobj.x<<"\n";     // 0
+
+  
+    cout<<"End of main\n";
+
+    return 0;
+}
