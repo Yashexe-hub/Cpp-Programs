1ea673242873c65078c11847bf954a827abcd5e2
Yash Thakur <yashthakur200529@gmail.com>
2026-05-08 17:51:09 +0530

    Add files via upload

--- RefrenceDemoX.cpp
+++ RefrenceDemoX.cpp
@@ -5,6 +5,20 @@ int main()
 {
     //We can create one refrence to one variable
 
+    int no = 11;
+    int &x = no;
+
+    //We can create multiple refrences for single variable
+    int &y = no;
+
+    //We can create refrence to refrence
+    int &z = y;
+
+    //We can create refrence to pointer
+    int *p = &no;
+    int *(&q) = p;
+
+
     return 0;
 
 }
